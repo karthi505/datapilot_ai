480952a0b95a58392629aa24ad0255daf61f5e14
Karthik Krishna S <karthikxkrishnas@gmail.com>
2026-07-30 15:24:35 +0530

    patch method is now allowed by cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", origin || "*");
     res.header(
       "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
+      "GET, POST, PUT, DELETE, OPTIONS, PATCH"
     );
     res.header(
       "Access-Control-Allow-Headers",
